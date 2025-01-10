3a5421c6d8e6783b8ee73f3aad3bef8800f21b13
igotyabingo <jenifer7933@snu.ac.kr>
2025-01-10 13:40:11 +0900

    Initialize

---
+++ 1+rockscissor/rockscissor/rockscissorTests/rockscissorTests.swift
@@ -0,0 +1,17 @@
+//
+//  rockscissorTests.swift
+//  rockscissorTests
+//
+//  Created by woo on 1/10/25.
+//
+
+import Testing
+@testable import rockscissor
+
+struct rockscissorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
