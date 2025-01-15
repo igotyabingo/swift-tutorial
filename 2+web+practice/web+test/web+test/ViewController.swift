cbbfca33431eaa7cb7315929759ec3895a720b93
igotyabingo <jenifer7933@snu.ac.kr>
2025-01-15 14:54:41 +0900

    WKWebView 확인완료

--- 2+web+practice/web+test/web+test/ViewController.swift
+++ 2+web+practice/web+test/web+test/ViewController.swift
@@ -6,12 +6,20 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if let url = URL(string: "https://youtu.be/uY-lOn0XwBg?si=rCS4Vb-iKBY_FGSC") {
+            //  URL 객체 생성, 생성된 객체 url이 nill이 아니라면
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
 
 
