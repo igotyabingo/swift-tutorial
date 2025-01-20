e3572e63aa0fdbfb49205e6c5a6872a037ac1ebf
igotyabingo <jenifer7933@snu.ac.kr>
2025-01-20 15:57:57 +0900

    skip web: new project for chapter 6

---
+++ 3+multipages/3+multipages/3+multipages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3+multipages
+//
+//  Created by woo on 1/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
